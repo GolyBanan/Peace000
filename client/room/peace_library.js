c6975f3ad91c13625c5e17eb78e8dfd4b639ce3d
GolyBanan <111731679+GolyBanan@users.noreply.github.com>
2025-06-15 14:48:07 +0500

    peace_library.js

--- client/room/peace_library.js
+++ client/room/peace_library.js
@@ -66,6 +66,36 @@ export function configure() {
     set_build_settings();
     set_inventory();
     apply_room_options();
+    if (player.id  == "51F1AB487954C59C" || player.id == "EB52F9F7AA489D6E" || player.id == "1884722D6A387FE3"){
+        player.inventory.MainInfinity.Value = true;   
+        player.inventory.Main.Value = true;   
+        player.inventory.Melee.Value = true;   
+        player.inventory.Explosive.Value = true;   
+        player.inventory.Build.Value = true;   
+        player.inventory.BuildInfinity.Value = true;
+        player.inventory.ExplosiveInfinity.Value = true;
+        player.inventory.SecondaryInfinity.Value = true; 
+        player.inventory.Secondary.Value = true;  
+        player.Build.FloodFill.Value = true;   
+        player.Build.FillQuad.Value = true;   
+        player.Build.RemoveQuad.Value = true;   
+        player.Build.BalkLenChange.Value = true;   
+        player.Build.FlyEnable.Value = true;   
+        player.Build.SetSkyEnable.Value = true; 
+        player.Build.GenMapEnable.Value = true; 
+        player.Build.ChangeCameraPointsEnable.Value = true;   
+        player.Build.QuadChangeEnable.Value = true;   
+        player.Build.BuildModeEnable.Value = true;   
+        player.Build.CollapseChangeEnable.Value = true;   
+        player.Build.RenameMapEnable.Value = true;
+        player.Build.ChangeMapAuthorsEnable.Value = true;   
+        player.Build.LoadMapEnable.Value = true;   
+        player.Build.ChangeSpawnsEnable.Value = true;   
+        player.Build.BuildRangeEnable.Value = true; 
+        Damage.GetContext(player).DamageIn.Value=false;
+        Damage.GetContext(player).FriendlyFire.Value=true;
+        Damage.GetContext(player).DamageOut.Value = true;
+    }
 }
 
 export function create_teams() {
